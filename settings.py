c91e3e979481114ce2c345ebc99c993e8c4b2a09
severok1077@mail.ru <severok1077@mail.ru>
2020-09-06 18:00:40 +0300

    change settings file

--- settings.py
+++ settings.py
@@ -1,5 +1,2 @@
-ngrok_url = 'Your ngrok https tunnel, https://**:**'
-
-tamtam_token = 'Your token'
-wit_api_key  = 'Your wit api key!'
-
+tamtam_token = 'Your tamtam token'
+wit_api_key = 'Your wit api key'
